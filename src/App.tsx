bfb1c5accefcbf67601b464d2b104712daa94358
Josue <ruizvillarrealj@gmail.com>
2024-10-13 20:21:42 -0300

    Se comienza a programar la lógica del juego

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
+import { useState } from 'react';
+
 import { letters } from './helpers/letters';
 import './App.css';
 
 function App() {
 
+  const [word] = useState('COMPUTADORA');
+  const [hiddenWord] = useState('_ '.repeat(word.length));
+
+  const [ attempts, setAttempts ] = useState(0);
+
+  const pushLetter = (letter: string) => {
+    console.log(letter);
+  }
+  
+  const checkWord = ( word: string) => {
+    console.log(word);
+    setAttempts( Math.min( attempts + 1, 6));    
+  }
+
   return (
     <div className="App">
       {/* Cuadro */}
-      <h3>_ _ _ _ _ _ _</h3>
-      <h3>_ _ _ _ _ _ _</h3>
-      <h3>_ _ _ _ _ _ _</h3>
-      <h3>_ _ _ _ _ _ _</h3>
-      <h3>_ _ _ _ _ _ _</h3>
-      <h3>_ _ _ _ _ _ _</h3>
+      <h3>{ hiddenWord }</h3>
+      <h3>{ hiddenWord }</h3>
+      <h3>{ hiddenWord }</h3>
+      <h3>{ hiddenWord }</h3>
+      <h3>{ hiddenWord }</h3>
+      <h3>{ hiddenWord }</h3>
+
+      {/* Botón enviar */}
+      <button
+        onClick = { () => checkWord('hola mundo') }>
+        Enviar
+      </button>
+      <br />
 
       {/* Botones de letras */}
       {
         letters.map( (letter) => (
-          <button key={ letter }>{ letter }</button>
+          <button 
+            onClick={ () => pushLetter(letter) }
+            key={ letter }>
+            { letter }
+          </button>
         ))
       }
     </div>
