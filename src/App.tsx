ca59c2820d1b2e50d83e55de923906a5ddeb8744
Josue <ruizvillarrealj@gmail.com>
2024-10-13 19:45:49 -0300

    Se construye plano del juego

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { letters } from './helpers/letters';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      {/* Cuadro */}
+      <h3>_ _ _ _ _ _ _</h3>
+      <h3>_ _ _ _ _ _ _</h3>
+      <h3>_ _ _ _ _ _ _</h3>
+      <h3>_ _ _ _ _ _ _</h3>
+      <h3>_ _ _ _ _ _ _</h3>
+      <h3>_ _ _ _ _ _ _</h3>
+
+      {/* Botones de letras */}
+      {
+        letters.map( (letter) => (
+          <button key={ letter }>{ letter }</button>
+        ))
+      }
     </div>
   )
 };
